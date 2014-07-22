1169e2c6034047b5a87c9bea04029188f5cd7852
贺晓聪 <xiaocong@gmail.com>
2014-07-22 12:17:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,19 @@ import os
 import time
 
 def main():
-    texts = []
     d().scroll.horiz.toBeginning()
-    while d(focused=True).exists and d(focused=True).text not in texts:
-        texts.append(d(focused=True).text)
+    pre = None
+    while d(focused=True).exists:
+        cur = d(focused=True).info
+        if cur == pre:
+            break
+        else:
+            pre = cur
         d(focused=True).click()
         time.sleep(1)
         d.press.home()
         d.press.right()
+        
             
 if __name__ == '__main__':
     for i in range(100000):
